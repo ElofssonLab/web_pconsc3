034f594983990a395c94a663cc5839abcf3fca26
Nanjiang Shu <nanjiang.shu@bils.se>
2016-05-18 11:15:30 +0200

    add week_beg_end() to myfunc lib

--- proj/pred/app/myfunc.py
+++ proj/pred/app/myfunc.py
@@ -2370,4 +2370,16 @@ def ReadNews(infile):#{{{
         print >> sys.stderr, "Failed to read newsfile %s"%(infile)
         return []
 #}}}
+def week_beg_end(day):#{{{
+    """
+    Given a date return the date of the 
+    beginning_of_week (Monday) and end_of_week
+    """
+    day_of_week = day.weekday()
+    to_beginning_of_week = datetime.timedelta(days=day_of_week)
+    beginning_of_week = day - to_beginning_of_week
+    to_end_of_week = datetime.timedelta(days=6 - day_of_week)
+    end_of_week = day + to_end_of_week
+    return (beginning_of_week, end_of_week)
+#}}}
 
