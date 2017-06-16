79ab28494c260667d1725447af90b902c2609e76
Nanjiang Shu <nanjiang.shu@bils.se>
2017-06-16 08:06:13 +0200

    fix a bug in settings

--- proj/pro_settings.py
+++ proj/pro_settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.append("%s/pred/app/"%(BASE_DIR))
 
