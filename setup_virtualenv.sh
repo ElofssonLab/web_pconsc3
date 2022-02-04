d039ff70011c870e1e4187bef6fffab93100ea8d
nanjiangshu <nanjiang.shu@gmail.com>
2022-02-04 22:01:41 +0100

    fix: update setup scripts

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -2,15 +2,18 @@
 # install virtualenv if not installed
 # first install dependencies
 # make sure virtualenv is installed with Python3
-#python3 -m pip install --upgrade pip
-#pip3 install virtualenv
+if [ ! $(type -P pip3) ] ;then
+    sudo python3 -m pip install --upgrade pip
+fi
+if [ ! $(type -P virtualenv) ] ;then
+    sudo pip3 install virtualenv
+fi
 # then install programs in the virtual environment
 mkdir -p ~/.virtualenvs
 rundir=`dirname $0`
 rundir=`readlink -f $rundir`
 cd $rundir
-exec_virtualenv=virtualenv
-eval "$exec_virtualenv env"
+virtualenv env
 source ./env/bin/activate
 
 pip3 install --ignore-installed -r requirements.txt
